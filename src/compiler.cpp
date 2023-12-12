d9a1414fd02348da4bd01dcd1834c00978f6124c
chan <13917164365@163.com>
2023-12-12 17:43:34 +0800

    updated to lab5

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -8,6 +8,9 @@
 #include "llvm_ir.h"
 #include "ast2llvm.h"
 #include "printLLVM.h"
+#include "ssa.h"
+#include "graph.hpp"
+#include "bg_llvm.h"
 
 #define YACCDEBUG 0
 
@@ -33,28 +36,34 @@ int main(int argc, char * argv[]) {
     col = 1;
 
     string input_name = argv[1];
-    auto dot_pos = input_name.rfind('.');
+    auto dot_pos = input_name.find('.');
     if(dot_pos == input_name.npos)
     {
         cout << "input error";
         return -1;
     }
     string file_name(input_name.substr(0,dot_pos));
-    cout << file_name << endl;
+    
     freopen(argv[1], "r", stdin);  
     ofstream ASTStream;
-    ASTStream.open(file_name+".ast");
+    // ASTStream.open(file_name+".ast");
+
     yyparse();
+    
     aroot = aA_Program(root);
-    print_aA_Program(aroot, ASTStream);
-    ASTStream.close();
+    // print_aA_Program(aroot, ASTStream);
+    // ASTStream.close();
 
     // check_Prog(&std::cout, aroot);
+
     ofstream LLVMStream;
     LLVMStream.open(file_name + ".ll");
     auto prog = ast2llvm(aroot);
+    prog=SSA(prog);
     printL_prog(LLVMStream,prog);
+    
     LLVMStream.close();
+    printf("exit\n");
     return 0;
 }
 
